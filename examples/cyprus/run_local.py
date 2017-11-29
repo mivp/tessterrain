bce2aa53076c37ec63d42383370dd6b69da9754e
Toan Nguyen <ntoand@gmail.com>
2017-11-29 11:22:25 +1100

    update cyprus

--- examples/cyprus/run_local.py
+++ examples/cyprus/run_local.py
@@ -17,8 +17,6 @@ data_height_scale = 1.0
 # model
 scene = getSceneManager()
 all = SceneNode.create("everything")
-# ui
-uim = UiModule.createAndInitialize()
 
 site_centers = {}
 
@@ -38,23 +36,40 @@ light2.setEnabled(True)
 
 # Camera
 cam = getDefaultCamera()
-cam.setPosition(Vector3(55875.2, 18618.9, 160089))
-cam.setOrientation(Quaternion(0, 0, 0, 1))
+cam.setPosition(Vector3(146047.75, 17604.45, 178648.63))
+cam.setOrientation(Quaternion(0.99, -0.14, -0.1, 0.01))
 cam.getController().setSpeed(4000)
 setNearFarZ(2, 400000)
 cam.addChild(light2)
 
 colormap = {}
 colormap[0] = '#ffffff'
-colormap[1] = '#ff0000'
-colormap[2] = '#ffff00'
-colormap[3] = '#ff00ff'
-colormap[4] = '#00ff00'
-colormap[5] = '#0000ff'
+colormap[1] = '#00008F'
+colormap[2] = '#0080FF'
+colormap[3] = '#80FF80'
+colormap[4] = '#FF8000'
+colormap[5] = '#800000'
+
+
+# ui
+uim = UiModule.createAndInitialize()
+imgplot = loadImage('terraindata/cyprus/colormap.jpg')
+hplot = Container.create(ContainerLayout.LayoutVertical, uim.getUi())
+hplot.setVisible(True)
+hplot.setStyle('fill: #aaaaaa80')
+hplot.setSize(Vector2(imgplot.getWidth(), imgplot.getHeight())) 
+#hplot.setAutosize(True)
+#hplot.setPosition(Vector3(10000, 200, 0))
+hplot.setPosition(Vector3(10, 10, 0))
+plotview = Image.create(hplot)
+plotview.setData(imgplot)
+#plotview.setPosition(Vector2(2, 2))
+#plotview.setSize(Vector2(imgplot.getWidth(), 64)) 
+
 
 
 def drawSite(site, options):
-    print site["Latitude"], site["Longitude"], site["Height"], site["Vessels"], site["Beads"]
+    print site["Tier"], site["Short"], site["Latitude"], site["Longitude"], site["Height"], site["Vessels"], site["Beads"]
     lat = float(site["Latitude"])
     lon = float(site["Longitude"])
     height = float(site["Height"]) + 200
@@ -62,6 +77,7 @@ def drawSite(site, options):
     vessel_c = int(min(vessel / 5, 5))
     bead = int(site["Beads"])
     bead_c = int(min(bead / 5, 5))
+    display = '(' + site["Tier"] + ') '  + site["Short"]
 
     # calculate position
     oneDegreeLength = 30.86666667 * 3647
@@ -69,6 +85,19 @@ def drawSite(site, options):
     z = -1 * (lat - ref_point['lat']) * oneDegreeLength * data_xz_scale[1];
     pos = Vector3(x, height, z)
     print 'pos: ', pos, vessel_c, bead_c
+
+    #text
+    text = Text3D.create('fonts/arial.ttf', 0.6, display)
+    text.setPosition(Vector3(0.5, 0.4, 0.5))
+    #text.setPosition(pos)
+    text.setFontResolution(120)
+    text.setColor(Color('green'))
+
+    center = SphereShape.create(0.5, 2)
+    center.setScale(Vector3(1000, 1000, 1000))
+    center.setPosition(pos)
+    center.setVisible(False)
+    center.addChild(text)
 
     size = 1000
     if vessel == 0 and bead == 0:
@@ -81,31 +110,37 @@ def drawSite(site, options):
 
     elif vessel > 0 and bead == 0:
         print 'vessel only'
-        sphere = SphereShape.create(0.5, 2)
-        sphere.setScale(Vector3(size, size, size))
-        sphere.setEffect('colored -d ' + colormap[vessel_c])
-        sphere.setVisible(True)
-        sphere.setPosition(pos)
+        cylinder = CylinderShape.create(1, 0.5, 0.5, 1, 16)
+        cylinder.pitch(radians(-90))
+        cylinder.setScale(Vector3(size, size, size))
+        cylinder.setEffect('colored -d ' + colormap[vessel_c])
+        cylinder.setVisible(True)
+        cylinder.setPosition(pos[0], pos[1], pos[2] + size/2)
+        
 
     elif vessel == 0 and bead > 0:
         print 'bead only'
-        box = BoxShape.create(1, 1, 1)
-        box.setScale(Vector3(size, size, size))
-        box.setPosition(Vector3(pos[0], pos[1] + size/2, pos[2]))
-        box.setEffect('colored -d ' + colormap[bead_c])
-
-    else:
-        print 'both'
         sphere = SphereShape.create(0.5, 2)
         sphere.setScale(Vector3(size, size, size))
-        sphere.setEffect('colored -d ' + colormap[vessel_c])
+        sphere.setEffect('colored -d ' + colormap[bead_c])
         sphere.setVisible(True)
-        sphere.setPosition(Vector3(pos[0] - size/2-100, pos[1] + size/2, pos[2]))
+        sphere.setPosition(pos[0], pos[1] + size/2, pos[2])
+        
+    else:
+        print 'both'
+        cylinder = CylinderShape.create(1, 0.5, 0.5, 1, 16)
+        cylinder.pitch(radians(-90))
+        cylinder.setScale(Vector3(size, size, size))
+        cylinder.setEffect('colored -d ' + colormap[vessel_c])
+        cylinder.setVisible(True)
+        cylinder.setPosition(Vector3(pos[0] - size/2-100, pos[1], pos[2]))
 
-        box = BoxShape.create(1, 1, 1)
-        box.setScale(Vector3(size, size, size))
-        box.setPosition(Vector3(pos[0] + size/2+100, pos[1] + size/2, pos[2]))
-        box.setEffect('colored -d ' + colormap[bead_c])
+        sphere = SphereShape.create(0.5, 2)
+        sphere.setScale(Vector3(size, size, size))
+        sphere.setEffect('colored -d ' + colormap[bead_c])
+        sphere.setVisible(True)
+        sphere.setPosition(Vector3(pos[0] + size/2+100, pos[1] + size/2, pos[2]))
+    
 
 
 # sites
